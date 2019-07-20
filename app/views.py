89a30589c6b7e2369cebec18a0637ea1373e3299
Kayrat <kaipov.kayrat@gmail.com>
2019-07-20 12:52:50 +0600

    gotoviy project

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 class FieldsListView(ListView):
     model = Post
     template_name = 'home.html'
+
+class FieldDetailView(DetailView): 
+    model = Post
+    template_name = 'field_detail.html'
+
+class FieldCreateView(CreateView): 
+    model = Post
+    template_name = 'field_new.html'
+    fields = ['title', 'price', 'body', 'adress']
