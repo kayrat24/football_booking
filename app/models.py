5292498286484e127546805a01c243c91961b769
kayrat24 <kaipov.kayrat@gmail.com>
2019-07-20 08:47:41 +0000

    Kayrat

--- app/models.py
+++ app/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.urls import reverse 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=0)
+    adress = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self): 
+        return reverse('field_detail', args=[str(self.id)])
